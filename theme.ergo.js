57924713d3637b8a124f105c5c749ec249999379
cmroanirgo <cmroanirgo@users.noreply.github.com>
2017-02-04 08:58:23 +1100

    added support for thumbnails and responsive images

--- theme.ergo.js
+++ theme.ergo.js
@@ -24,7 +24,19 @@ module.exports = {
 				return this.paginate.call(this, list, params, list_name);
 			else
 				return list; // do nothing, if paginate not available
-			},
+		},
+
+		// gracefully support thumbnail plugin for srcset & thumb filters
+		safe_srcset: function(image_name, params, data_name) {
+			if (!!this.srcset)
+				return this.srcset.call(this, image_name, params, data_name);
+			return '';
+		},
+		safe_thumb: function(image_name, params, data_name) {
+			if (!!this.thumb)
+				return this.thumb.call(this, image_name, params, data_name);
+			return image_name;
+		},
 	}
 };
 
